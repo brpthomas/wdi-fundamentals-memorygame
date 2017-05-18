cdfa09cc46bcdf65342c9ebee7586fd69359856b
brpthomas <superbadpeter@Peters-MacBook-Air.local>
2017-05-18 13:02:00 -0500

    updated game logic

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "jack";
-var cardFour = "ace";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var gameBoard = document.getElementById("gameBoard");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = "card";
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', isTwoCards);
+    gameBoard.appendChild(newCard);
+  }
+  };
+  function isTwoCards () {
+    cardInPlay.push(this.getAttribute('data-card'));
+    this.innerHTML = '<img src="' + this.getAttribute('data-card') + 'jpg">'
+    if (cardInPlay.length === 2) {
+      isMatch(cardInPlay);
+      cardInPlay = [];
+    }
+  };
+  var isMatch = function(array) {
+    (array [0] === array[1]) ? alert("You have found a match!") : alert ("Try Again!")
+    for (var i = 0; i < cards.length; i++) {
+      document.getElementsByClassName('card')[i].innerHTML = " ";
+    }
+  };
